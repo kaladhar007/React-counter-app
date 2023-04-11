ecc9a23bd0617431bc2a5820a95d4fff06d63b69
kaladhar <kaladhar0777@gmail.com>
2023-04-11 10:54:31 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Counter from './components/Counter'
+
+const App = props => {
+  return <Counter />
+}
+export default App
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,10 @@
+import {Component} from 'react'
+
+class Welcome extends Component {
+  render() {
+    const {name} = this.props
+    return <h1>{name} One of the richest man in the world</h1>
+  }
+}
+
+export default Welcome
